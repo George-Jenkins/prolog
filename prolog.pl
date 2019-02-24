f4b60ff3e26b42bc691cb4b4011555ae289c5023
George Jenkins <georgejenkins@Georges-MBP.fios-router.home>
2019-02-23 22:56:43 -0500

    adding min above min predicate and predicates that determine final answer given what other predicated determine

--- prolog.pl
+++ prolog.pl
@@ -21,3 +21,13 @@ adder2([X|L],Sum) :- adder2(L,SL), numTest2(X,A), Sum is A + SL.
 findSum(L,N) :- adder2(L,Sum), findAns(N,Sum).
 
 sum-up-numbers-general(L, N) :- findSum(L,N).
+
+
+
+
+runProgram(L1, L2, N, A) :- newList([],L1,Li1), newList([],L2,Li2), findMins(Li1,Li2,N,A).
+
+getAns(A) :- A=true, true.
+getAns(A) :- A=false, false.
+
+min-above-min(L1, L2, N) :- runProgram(L1, L2, N, A), !,getAns(A).
