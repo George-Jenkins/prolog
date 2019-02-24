ebd97b6d6b536e9ef59914cf1c6dc965dd541eb2
George Jenkins <georgejenkins@Georges-MBP.fios-router.home>
2019-02-23 22:46:28 -0500

    Added predicates that add numbers and makes sure the element is a number

--- prolog.pl
+++ prolog.pl
@@ -1,3 +1,10 @@
+numTest2(X,A) :- number(X), A is X.
+numTest2(X,A) :- is_list(X), adder2(X,A).
+numTest2(X,A) :- A is 0.
+
+adder2([ ],0).
+adder2([X|L],Sum) :- adder2(L,SL), numTest2(X,A), Sum is A + SL.
+
 findSum(L,N) :- adder2(L,Sum), findAns(N,Sum).
 
 sum-up-numbers-general(L, N) :- findSum(L,N).
